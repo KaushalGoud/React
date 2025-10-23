fba17fbef96a1c89f549be19b07ea1208677fc74
Kaushal Goud <kaushalgoud82@gmail.com>
2025-10-23 05:56:17 +0545

    laying

---
+++ All chapters/app.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+//React element
+// const Heading=React.createElement("div",{id:"parent"},"Hello I am inside div");
+// console.log(Heading)
+const Name = "Kaushal";
+const JsxElement = () => (
+  <h1>Hello world I am JSX different than react I am written by{Name}</h1>
+);
+const Heading = () => (
+  <div>
+    <JsxElement />
+    <h1>This is a Junctional react based component</h1>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Heading />);
